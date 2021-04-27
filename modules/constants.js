5e37d7342c0c6cf89c5c43b9b4a20bc1d4322351
Luca Joos <1a85ra7z@gmail.com>
2021-04-27 09:41:53 +0200

    Fix wrong path to resources in build

--- modules/constants.js
+++ modules/constants.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const r = {
     URL: app.isPackaged ? `file://${path.join(__dirname, '../build/index.html')}` : 'http://localhost:3000',
-    RESOURCES_PATH: path.join(__dirname, '../resources/')
+    RESOURCES_PATH: path.join(__dirname, `${app.isPackaged ? '../' : ''}../resources/`)
 };
 
 module.exports = r;
